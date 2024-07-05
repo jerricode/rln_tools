6e8f3a3d23dc12ad824880771a34df879a916f5c
jerrico <johnmerrico@gmail.com>
2024-07-05 15:41:11 -0700

    Create relion_plt_class_distributions.py

---
+++ rln_plt_class_distributions.py
@@ -0,0 +1,7 @@
+"""
+Plot the particle distribution between classes as a function of refinement iteration for a series of star files.
+"""
+
+import rln_starparser
+from matplotlib import pyplot as plt
+import numpy as np
