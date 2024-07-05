170b070c6b3f711b007d0b9c454d0e298f6eaa07
jerrico <johnmerrico@gmail.com>
2024-07-05 16:25:46 -0700

    Add metadata_column function to starparser; remove column extraction functionality from rln_plt_orientations

--- rln_starparser.py
+++ rln_starparser.py
@@ -1,10 +1,11 @@
 """
 
-Tools used to parse input STAR files from relion jobs for downstream use.
+Tools used to parse input STAR files from relion jobs and extract requested data for downstream use.
 
 """
 
 def parse_star(starfile) -> list:
+    
     # Open the star file, parse it and return as a list
     
     output = []
@@ -16,18 +17,29 @@ def parse_star(starfile) -> list:
     
     return output
 
-def metadata_index(starfile: list, metadata_label: str) -> int:
+def metadata_index(star: list, metadata_label: str) -> int:
     
     # Find index of metadata label of interest and convert to python numbering
     
     label = 0
     
-    for line in starfile:
-        if len(line) < 15 and len(line) > 1:
-            if line[0] == metadata_label:
-                label = int(line[1][1:])
+    for row in star:
+        if len(row) < 15 and len(row) > 1:
+            if row[0] == metadata_label:
+                label = int(row[1][1:])
     
     return label-1
+
+def metadata_column(star: list, metadata_label: str) -> list:
+
+    label = metadata_index(star, metadata_label)
+    output = []
+
+    for row in star:
+        if len(row) > 10:
+            output.append(row[label])
+
+    return output
 
 if __name__ == "__main__":
     pass
