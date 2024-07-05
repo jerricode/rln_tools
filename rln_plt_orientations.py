4084941af0c24163d88bcbee449a05db6776695f
jerrico <johnmerrico@gmail.com>
2024-07-05 16:25:46 -0700

    Add metadata_column function to starparser; remove column extraction functionality from rln_plt_orientations

--- rln_plt_orientations.py
+++ rln_plt_orientations.py
@@ -9,7 +9,7 @@ from matplotlib import pyplot as plt
 from sys import argv
 import rln_starparser
 
-def radians(degree: float) -> float:
+def radian(degree: float) -> float:
     return degree * (np.pi / 180)
 
 def plot(angles: list) -> plt:
@@ -32,7 +32,7 @@ def plot(angles: list) -> plt:
     plt.xlabel('Azimuth')
     plt.ylabel('Elevation')
     plt.tight_layout()
-    plt.savefig('orientations.svg', format='svg', metadata={'Description' : f'Generated from {argv[1]}'})
+    # plt.savefig('orientations.svg', format='svg', metadata={'Description' : f'Generated from {argv[1]}'})
     plt.show()
 
 def plot_orientations():
@@ -43,18 +43,18 @@ def plot_orientations():
         print("Please specify an input STAR file after the program name")
         exit()
     
-    # Get the metadata indices of interest for plotting
-    rot = rln_starparser.metadata_index(star, '_rlnAngleRot')
-    tilt = rln_starparser.metadata_index(star, '_rlnAngleTilt')
+    # Get the rot and tilt angles from the STAR file
+    rot = rln_starparser.metadata_column(star, '_rlnAngleRot')
+    tilt = rln_starparser.metadata_column(star, '_rlnAngleTilt')
+    angles = zip(rot, tilt)
 
-    # Extract angles of interest (rotation, tilt), convert to radians, output as a tuple
-    angles_radians = []
-    for row in star:
-        if len(row) > 10: # Skip the header portion of the file
-            angles_radians.append((radians(float(row[rot])), (radians(float(row[tilt])-90))))
+    # Convert rotations and tilts to radians, output as tuple
+    radians = []
+    for rot, tilt in angles:
+        radians.append((radian(float(rot)), radian(float(tilt))-(np.pi/2))) # Shift tilt 90 degrees 
     
     # Plot the converted angles as a hexbin heatmap
-    plot(angles_radians)
+    plot(radians)
 
 if __name__ == "__main__":
     plot_orientations()
