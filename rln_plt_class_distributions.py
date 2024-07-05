4f86eb9f43ee83478ccafe592ffb359446a33a7e
jerrico <johnmerrico@gmail.com>
2024-07-05 16:36:03 -0700

    Add description and module imports

--- rln_plt_class_distributions.py
+++ rln_plt_class_distributions.py
@@ -1,7 +1,10 @@
 """
+
 Plot the particle distribution between classes as a function of refinement iteration for a series of star files.
+
 """
 
 import rln_starparser
 from matplotlib import pyplot as plt
 import numpy as np
+import os
